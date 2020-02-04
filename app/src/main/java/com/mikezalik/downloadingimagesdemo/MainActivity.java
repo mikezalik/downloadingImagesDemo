b9e848fbaf8bbcb7ce465dfbd1fa39fc27c567a1
mikezalik <mike.zalik@gmail.com>
2020-02-04 09:55:58 -0800

    Adds XML layout and onClick method for button.

--- app/src/main/java/com/mikezalik/downloadingimagesdemo/MainActivity.java
+++ app/src/main/java/com/mikezalik/downloadingimagesdemo/MainActivity.java
@@ -3,8 +3,16 @@ package com.mikezalik.downloadingimagesdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    public void downloadImage (View view) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
